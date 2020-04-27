fa3012a5bbc6dd29a0cea703ce8d005addec8aaf
Timur Minnakhmetov <timvir@gmail.com>
2020-04-26 18:32:04 -0700

    Add files

---
+++ brew.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Save Homebrew’s installed location.
+BREW_PREFIX=$(brew --prefix)
+
+#Install everything from Brewfile
+brew bundle install
+
+#Cleanup
+brew cleanup
